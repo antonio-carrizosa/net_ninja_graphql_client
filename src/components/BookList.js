7bb06df5c3253136ac2d2e1340f6b9d985f1e4ac
Antonio Carrizosa <jantoniocarrizosa@gmail.com>
2022-02-17 09:54:39 -0600

    Can display book details when a book is selected

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,30 @@
 import { useQuery } from "@apollo/client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 const BookList = () => {
   const { loading, error, data } = useQuery(getBooksQuery);
+  const [selectedBook, setSelectedBook] = useState(null);
 
   return (
     <div>
+      
+      <ul id="book-list">
       {loading && <div> Loading... </div>}
 
-      {data && (
-        <ul id="book-list">
-          {data.books.map((book) => (
-            <li key={book.id}> {book.name} </li>
-          ))}
-        </ul>
-      )}
+      {data && 
+          data.books.map((book) => (
+            <li key={book.id} onClick={ () => setSelectedBook(book.id) }> {book.name} </li>
+          ))
+          }          
+      </ul>
 
+      <div id="book-details">      
+      { !selectedBook && <div> No book selected ... </div> }
+      { selectedBook && <BookDetails bookId={selectedBook} />}
+      </div>
+      
       {error && !loading && <p> Error: {error.message} </p>}
     </div>
   );
